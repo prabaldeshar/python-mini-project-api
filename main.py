85983cb03edd569c5864e916a11a70000a3db8bc
Prabal Deshar <prabaldeshar@gmail.com>
2022-01-13 21:50:22 +0545

    Private and Public keys added for RSA encryption

--- main.py
+++ main.py
@@ -33,16 +33,17 @@ def token_required(f):
     @wraps(f)
     def decorated(*args, **kwargs):
         token = None
+        public_key = open('key/jwt-key.pub').read()
         if 'x-access-token' in request.headers:
             token = request.headers['x-access-token']
-            print(f'printing token--->{token}')
+            print(f'printing new token--->{type(token)}')
         if not token:
             return jsonify({'message': 'Token is missing'}), 401
         try:
-            data = jwt.decode(token, app.config['SECRET_KEY'])
-            current_user = User.query.filter_by(public_id=data['public_id'])
-        except:
-            return jsonify({'message': 'Token is invaliid'}), 401
+            data = jwt.decode(token, public_key, algorithms=["RS256"])
+            current_user = User.query.filter_by(public_id=data['public_id']).first()
+        except Exception as e:
+            return jsonify({'message': str(e)}), 401
 
         return f(current_user, *args, **kwargs)   
 
@@ -67,18 +68,17 @@ def get_all_users(current_user):
     
     return jsonify({'users': output})
 
-@app.route('/user/<public_id>', methods=['GET'])
+@app.route('/user/profile/', methods=['GET'])
 @token_required
-def get_one_user(current_user, public_id):
-    user = User.query.filter_by(public_id=public_id).first()
-    if not user:
+def get_one_user(current_user):
+    # user = User.query.filter_by(public_id=public_id).first()
+    if not current_user:
         return jsonify({'message': 'No user found'})
     
+    # print(f"Printing current user{current_user.name}")
     user_data = {}
-    user_data['public_id'] = user.public_id
-    user_data['name'] = user.name
-    user_data['password'] = user.password
-    user_data['email'] = user.email
+    user_data['name'] = current_user.name
+    user_data['email'] = current_user.email
 
     return jsonify({'user': user_data})
 
@@ -111,6 +111,7 @@ def delete_user(current_user, public_id):
 @app.route('/login')
 def login():
     auth = request.authorization
+    private_key = open('key/jwt-key').read()
     if not auth or not auth.username or not auth.password:
         return make_response('Could not verify', 401, {'WWW-Authenticate': 'Basic realm="Login required!"'})
     
@@ -120,7 +121,7 @@ def login():
         return make_response('Could not verify', 401, {'WWW-Authenticate': 'Basic realm="Login required!"'})
     
     if check_password_hash(user.password, auth.password):
-        token = jwt.encode({'public_id' : user.public_id, 'exp' : datetime.datetime.utcnow() + datetime.timedelta(minutes=30)}, app.config['SECRET_KEY']) 
+        token = jwt.encode({'public_id' : user.public_id}, private_key, algorithm="RS256")
         return jsonify({'token': token})
 
     return make_response('Could not verify', 401, {'WWW-Authenticate': 'Basic realm="Login required!"'})
